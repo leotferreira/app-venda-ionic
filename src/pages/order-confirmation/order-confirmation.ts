a42946f12438549c737e64285667cfbba461941b
leotferreira <leo.teixeira27@gmail.com>
2019-04-02 23:37:20 -0300

    Informando pedido registrado ao usuario

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -1,83 +1,97 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { PedidoDTO } from '../../models/pedido.dto';
-import { CartItem } from '../../models/cart-item';
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
-import { CartService } from '../../services/domain/cart.service';
-import { ClienteDTO } from '../../models/cliente.dto';
-import { EnderecoDTO } from '../../models/endereco.dto';
-import { ClienteService } from '../../services/domain/cliente.service';
-import { PedidoService } from '../../services/domain/pedido.service';
+  import { Component } from '@angular/core';
+  import { IonicPage, NavController, NavParams } from 'ionic-angular';
+  import { PedidoDTO } from '../../models/pedido.dto';
+  import { CartItem } from '../../models/cart-item';
+  import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+  import { CartService } from '../../services/domain/cart.service';
+  import { ClienteDTO } from '../../models/cliente.dto';
+  import { EnderecoDTO } from '../../models/endereco.dto';
+  import { ClienteService } from '../../services/domain/cliente.service';
+  import { PedidoService } from '../../services/domain/pedido.service';
 
 
-@IonicPage()
-@Component({
-  selector: 'page-order-confirmation',
-  templateUrl: 'order-confirmation.html',
-})
-export class OrderConfirmationPage {
+  @IonicPage()
+  @Component({
+    selector: 'page-order-confirmation',
+      templateUrl: 'order-confirmation.html',
+  })
+  export class OrderConfirmationPage {
 
-  pedido: PedidoDTO;
-  cartItems: CartItem[];
-  cliente: ClienteDTO;
-  endereco: EnderecoDTO
+    pedido: PedidoDTO;
+    cartItems: CartItem[];
+    cliente: ClienteDTO;
+    endereco: EnderecoDTO;
+    codpedido: string;
 
-  constructor(
-    public navCtrl: NavController, 
-    public navParams: NavParams,
-    public cartService: CartService,
-    public clienteService: ClienteService,
-    public pedidoService: PedidoService) {
+    constructor(
+      public navCtrl: NavController, 
+      public navParams: NavParams,
+      public cartService: CartService,
+      public clienteService: ClienteService,
+      public pedidoService: PedidoService) {
 
-    this.pedido = this.navParams.get('pedido');
-
-  }
-
-  ionViewDidLoad() {
-   this.cartItems = this.cartService.getCart().items;
-
-   this.clienteService.findById(this.pedido.cliente.id)
-    .subscribe(response => {
-      this.cliente = response as ClienteDTO;
-      this.endereco = this.findEndereco(this.pedido.enderecoDeEntrega.id, response['enderecos']);
-
-    },
-     error => {
-       this.navCtrl.setRoot('HomePage');
-
-     });
-
-  }
-
-
-  private findEndereco(id: string, list: EnderecoDTO[]) : EnderecoDTO {
-    let position = list.findIndex(x => x.id == id);
-    return list[position];
-
-  }
-
-  total() {
-
-    this.cartService.total();
-  }
-
-    back() {
-  this.navCtrl.setRoot('CartPage');
+      this.pedido = this.navParams.get('pedido');
 
     }
 
-  checkout() {
-    this.pedidoService.insert(this.pedido)
-    .subscribe(response => {
-      this.cartService.createOrClearCart();
-      console.log(response.headers.get('location'));
-    },
+    ionViewDidLoad() {
+    this.cartItems = this.cartService.getCart().items;
+
+    this.clienteService.findById(this.pedido.cliente.id)
+      .subscribe(response => {
+        this.cliente = response as ClienteDTO;
+          this.endereco = this.findEndereco(this.pedido.enderecoDeEntrega.id, response['enderecos']);
+
+      },
       error => {
-        if (error.status == 403 ) {
-          this.navCtrl.setRoot('HomePage');
+        this.navCtrl.setRoot('HomePage');
 
-        }
       });
-  }
 
-}
+    }
+
+
+    private findEndereco(id: string, list: EnderecoDTO[]) : EnderecoDTO {
+      let position = list.findIndex(x => x.id == id);
+        return list[position];
+
+    }
+
+      total() {
+        this.cartService.total();
+    }
+
+      back() {
+        this.navCtrl.setRoot('CartPage');
+
+      }
+
+      home(){
+        this.navCtrl.setRoot('CategoriasPage');
+
+
+      }
+
+
+
+    checkout() {
+      this.pedidoService.insert(this.pedido)
+      .subscribe(response => {
+        this.cartService.createOrClearCart();
+          this.codpedido = this.extractId(response.headers.get('location'));
+      },
+        error => {
+          if (error.status == 403 ) {
+            this.navCtrl.setRoot('HomePage');
+
+          }
+        });
+    }
+
+    private extractId(location : string) : string {
+      let position = location.lastIndexOf('/');
+        return location.substring(position + 1, location.length);
+
+    }
+
+  }
